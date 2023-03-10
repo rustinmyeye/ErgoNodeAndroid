18ab066e0e74b2d52307730be66ce78daadd4f12
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-09 21:10:55 -0500

    Update alpine-ergo.sh

--- alpine-ergo.sh
+++ alpine-ergo.sh
@@ -7,13 +7,8 @@ TARBALL_URL="https://github.com/termux/proot-distro/releases/download/v1.2-alpin
 TARBALL_SHA256="0e8258cc599a86be428228dd5ea9217959f2c77873790bec645c05899f252c5e"
 
 distro_setup() {
-
-        # Run command within proot'ed environment with
-        # run_proot_cmd function.
-        # Uncomment this to run 'apk upgrade' during installation.
         run_proot_cmd apk upgrade
         run_proot_cmd apk add openjdk11
         run_proot_cmd apk add wget
         run_proot_cmd apk add python3
-        :
 }
