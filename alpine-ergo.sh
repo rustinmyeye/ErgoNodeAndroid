265462a53deb0c96fc6239c35294662260e00b83
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-09 21:02:46 -0500

    Update alpine-ergo.sh

--- alpine-ergo.sh
+++ alpine-ergo.sh
@@ -1,53 +1,13 @@
-##
-## Plug-in for installing Alpine Linux for Ergo Node.
-##
-
 DISTRO_NAME="alpine-ergo"
 
-# You can override a CPU architecture to let distribution
-# be executed by QEMU (user-mode).
-#
-# You can specify the following values here:
-#
-#  * aarch64: AArch64 (ARM64, 64bit ARM)
-#  * armv7l:  ARM (32bit)
-#  * i686:    x86 (32bit)
-#  * x86_64:  x86 (64bit)
-#
-# Default value is set by proot-distro script and is equal
-# to the CPU architecture of your device (uname -m).
-#DISTRO_ARCH=$(uname -m)
+DISTRO_ARCH=aarch64
 
-# Returns download URL and SHA-256 of file in this format:
-# SHA-256|FILE-NAME
-get_download_url() {
-        local rootfs
-        local sha256
+DISTRO_COMMENT="Alpine Linux setup for Ergo Node"
 
-        case "$DISTRO_ARCH" in
-                aarch64)
-                        rootfs="https://github.com/termux/proot-distro/releases/download/v1.2-alpine-rootfs/alpine-minirootfs-3.13.1-aarch64.tar.gz"
-                        sha256="0e8258cc599a86be428228dd5ea9217959f2c77873790bec645c05899f252c5e"
-                        ;;
-                armv7l|armv8l)
-                        rootfs="https://github.com/termux/proot-distro/releases/download/v1.2-alpine-rootfs/alpine-minirootfs-3.13.1-armv7.tar.gz"
-                        sha256="71bedb0810b656377192f4a239126f452a016d7a33e28f8ad8f46e9b0e7d8028"
-                        ;;
-                i686)
-                        rootfs="https://github.com/termux/proot-distro/releases/download/v1.2-alpine-rootfs/alpine-minirootfs-3.13.1-x86.tar.gz"
-                        sha256="6884df8f8e44d462a9723e412f0b76124019683d98afe448a8767dc5f5f3a7e0"
-                        ;;
-                x86_64)
-                        rootfs="https://github.com/termux/proot-distro/releases/download/v1.2-alpine-rootfs/alpine-minirootfs-3.13.1-x86_64.tar.gz"
-                        sha256="1ab0a7c05107c4504018be170d937c07e35283b7a3f153886c8c93162dfbf4af"
-                        ;;
-        esac
+TARBALL_URL['aarch64']="https://github.com/termux/proot-distro/releases/download/v1.2-alpine-rootfs/alpine-minirootfs-3.13.1-aarch64.tar.gz"
 
-        echo "${sha256}|${rootfs}"
-}
+TARBALL_SHA256['aarch64']="0e8258cc599a86be428228dd5ea9217959f2c77873790bec645c05899f252c5e"
 
-# Define here additional steps which should be executed
-# for configuration.
 distro_setup() {
 
         # Run command within proot'ed environment with
