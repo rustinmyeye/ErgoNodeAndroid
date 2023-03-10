919c0bd2d2e90a420867978740940209d063d374
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-09 21:25:40 -0500

    Update alpine-ergo.sh

--- alpine-ergo.sh
+++ alpine-ergo.sh
@@ -1,7 +1,20 @@
 DISTRO_NAME="alpine-ergo"
+
 DISTRO_COMMENT="Alpine Linux setup for Ergo Node"
-TARBALL_URL['aarch64']"https://github.com/termux/proot-distro/releases/download/v1.2-alpine-rootfs/alpine-minirootfs-3.13.1-aarch64.tar.gz"
-TTARBALL_SHA256['aarch64']="0e8258cc599a86be428228dd5ea9217959f2c77873790bec645c05899f252c5e"
+
+get_download_url() {
+        local rootfs
+        local sha256
+
+        case "$DISTRO_ARCH" in
+                aarch64)
+                        rootfs="https://github.com/termux/proot-distro/releases/download/v1.2-alpine-rootfs/alpine-minirootfs-3.13.1-aarch64.tar.gz"
+                        sha256="0e8258cc599a86be428228dd5ea9217959f2c77873790bec645c05899f252c5e"
+                        esac
+
+        echo "${sha256}|${rootfs}"
+}
+
 
 distro_setup() {
         run_proot_cmd apk upgrade
