376976903ad2bc36c1d31a4a70499be4610dfa7b
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-12 16:48:42 -0400

    Update ergo-startup.sh

--- ergo-startup.sh
+++ ergo-startup.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-#OS=$(uname -m)
-
-## Create directory
-[ -d ergo ] || mkdir ergo && cd ergo
-
-## Initial Minimal Config
+## Minimal Config
 echo "
     ergo {
         node {
@@ -14,7 +9,6 @@ echo "
 
     }" > ergo.conf
         
-
 ## Download .jar
 echo "- Retrieving latest node release.."
 LATEST_ERGO_RELEASE=$(curl -s "https://api.github.com/repos/ergoplatform/ergo/releases/latest" | awk -F '"' '/tag_name/{print $4}')
@@ -23,15 +17,9 @@ ERGO_DOWNLOAD_URL=https://github.com/ergoplatform/ergo/releases/download/${LATES
 echo "- Downloading Latest known Ergo release: ${LATEST_ERGO_RELEASE}."
 curl --silent -L ${ERGO_DOWNLOAD_URL} --output ergo.jar
 
-## Start the node.
-java -jar -Xmx3G ergo.jar --mainnet -c ergo.conf > server.log 2>&1 &
-echo "#### Waiting for a response from the server. ####"
-while ! curl --output /dev/null --silent --head --fail http://localhost:9053; do sleep 1 && echo -n '.';  done;  # wait for node be ready with progress bar
-
 ## Start node
-java -jar -Xmx2G ergo.jar --mainnet -c ergo.conf > server.log 2>&1 &
-
-echo "The node is now syncing"
 echo "Starting the node..."
 echo "Monitor the .log files for any errors"
 echo "Please visit https://127.0.0.0.9053/panel to view sync progress." 
+
+
