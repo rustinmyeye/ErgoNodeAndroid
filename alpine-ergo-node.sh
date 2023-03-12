8dbd09ded11af1a4b3b1d007658f98b198c5c7e7
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-12 15:00:18 -0400

    Update alpine-ergo-node.sh

--- alpine-ergo-node.sh
+++ alpine-ergo-node.sh
@@ -2,3 +2,11 @@
 
 #download things
 apk add openjdk11 wget python3
+
+#create startup script
+cd ..
+cd etc
+cd profile.d 
+curl https://raw.githubusercontent.com/glasgowm148/ergoscripts/main/sh/super-simple.sh >> ergo.sh
+chmod +x ergo.sh
+
