2a0f5bb6179e9226b5de7a674a490d87774c7eda
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-13 10:44:38 -0400

    Update alpine-ergo-setup.sh

--- alpine-ergo-setup.sh
+++ alpine-ergo-setup.sh
@@ -2,6 +2,7 @@
 
 #download proot-distro
 apt install proot-distro -y
+clear
 
 #download alpine-ergo.sh setup plugin for proot-distro
 cd ..
@@ -12,9 +13,11 @@ curl -s https://raw.githubusercontent.com/rustinmyeye/ErgoNodeAndroid/master/alp
 
 #install alpine linux with alpine-ergo plugin with proot-distro
 proot-distro install alpine-ergo
+clear
 
 #run alpine linux and start node setup
 proot-distro login alpine-ergo --  bash <(curl -s https://raw.githubusercontent.com/rustinmyeye/ErgoNodeAndroid/master/alpine-ergo-node.sh)
+clear
 proot-distro login alpine-ergo 
 
-clear
+
