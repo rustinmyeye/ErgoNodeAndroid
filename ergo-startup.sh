fd4533527ff8150c18b776af97d0f85087c0495c
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-13 12:03:01 -0400

    Update ergo-startup.sh

--- ergo-startup.sh
+++ ergo-startup.sh
@@ -179,7 +179,9 @@ echo "
 while true  
 do
           java -jar -Xmx1G ergo.jar --mainnet -c ergo.conf > server.log 2>&1 &
-            sleep 100
+            echo "#### Waiting for a response from the server. ####"
+while ! curl --output /dev/null --silent --head --fail http://localhost:9053; do sleep 1 && echo -n '.';  done;  # wait for node be ready with progress bar
+        sleep 100
     done" > start.sh
     
 chmod +x start.sh
@@ -191,16 +193,6 @@ LATEST_ERGO_RELEASE_NUMBERS=$(echo ${LATEST_ERGO_RELEASE} | cut -c 2-)
 ERGO_DOWNLOAD_URL=https://github.com/ergoplatform/ergo/releases/download/${LATEST_ERGO_RELEASE}/ergo-${LATEST_ERGO_RELEASE_NUMBERS}.jar
 echo "- Downloading Latest known Ergo release: ${LATEST_ERGO_RELEASE}."
 curl --silent -L ${ERGO_DOWNLOAD_URL} --output ergo.jar
-
-## Prepare node
-
-java -jar -Xmx3G ergo.jar --mainnet -c ergo.conf > server.log 2>&1 &
-echo "#### Waiting for a response from the server. ####"
-while ! curl --output /dev/null --silent --head --fail http://localhost:9053; do sleep 1 && echo -n '.';  done;  # wait for node be ready with progress bar
-
-## Kill
-#curl -X POST --max-time 10 "http://127.0.0.1:9053/node/shutdown" -H "api_key: $API_KEY"
-killall -9 java 2>/dev/null
 
 ##Start node
 echo "
