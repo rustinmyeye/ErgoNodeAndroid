be53b5d3c674967a2fb7ae655976b85a92c71c7a
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-13 16:12:30 -0400

    Update alpine-ergo-node.sh

--- alpine-ergo-node.sh
+++ alpine-ergo-node.sh
@@ -2,7 +2,7 @@
 
 #download things
 echo "Downloading stuff... please wait :)"
-apk --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ add android-tools --quiet
+#apk --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ add android-tools --quiet
 apk add openjdk11 python3 tmux curl --quiet
 
 #create startup script
