44ae7704dbe2ef88b8b391ace6d0a50245364d53
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-03-13 20:01:32 -0400

    Update defaults.kt

--- app/src/main/java/io/neoterm/component/config/defaults.kt
+++ app/src/main/java/io/neoterm/component/config/defaults.kt
@@ -43,7 +43,7 @@ object NeoTermPath {
   const val SOURCE_FILE = "$USR_PATH/etc/apt/sources.list"
   const val PACKAGE_LIST_DIR = "$USR_PATH/var/lib/apt/lists"
 
-  private const val SOURCE = "https://github.com/rustinmyeye/ergonode-repo-test/blob/main"
+  private const val SOURCE = "https://raw.githubusercontent.com/NeoTerm/NeoTerm-repo/main"
 
   val DEFAULT_MAIN_PACKAGE_SOURCE: String
 
