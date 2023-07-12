fcebf405c32d60ede83d1270914997a0c1fc0a33
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2023-07-12 10:20:54 -0400

    Update alpine-node-install.sh

--- alpine-node-install.sh
+++ alpine-node-install.sh
@@ -23,21 +23,10 @@ set_environment(){
     
 set_configuration(){
         echo "ergo {
-  ...
- node {
-  ...
-    utxo {
-        # Download and apply UTXO set snapshot and full-blocks after that
-        utxoBootstrap = true
-
-        # how many utxo set snapshots to store, 0 means that they are not stored at all
-        storingUtxoSnapshots = 2
-
-        # how many utxo set snapshots for a height with same id we need to find in p2p network
-        # in order to start downloading it
-        p2pUtxoSnapshots = 2
+    node {
+        mining = false
     }
-  }
+}
 
 scorex {
     restApi {
