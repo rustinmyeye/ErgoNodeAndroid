e5a1ccbc862715f5751cc2253ab8aff2c04023d7
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2025-03-05 09:30:13 -0500

    Update alpine-ergo-setup.sh

--- alpine-ergo-setup.sh
+++ alpine-ergo-setup.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
-
-#apt update -y
-#apt upgrade -y
-
-#install dependencies
-
-pkg install -y bash bzip2 coreutils curl  findutils gzip ncurses-utils proot tar xz-utils git proot-distro
-
-#download alpine-ergo.sh setup plugin forproot-distro
-
-cd ..
-cd usr
-cd etc
-cd proot-distro
-curl -s https://raw.githubusercontent.com/rustinmyeye/ErgoNodeAndroid/master/alpine-ergo.sh >> alpine-ergo.sh
+curl -s https://raw.githubusercontent.com/rustinmyeye/ErgoNodeAndroid/refs/heads/master/init-alpine.sh >> init-alpine.sh
+chmod +x init-alpine.sh
+curl -s https://raw.githubusercontent.com/rustinmyeye/ErgoNodeAndroid/refs/heads/master/run-alpine.sh >> run-alpine.sh
+chmod +x run-alpine.sh
+./init-alpine.sh
 clear
-
-#install alpine linux with alpine plugin with proot-distro
-
-proot-distro install alpine
-clear
-
-#run alpine linux and start node setup
-
-proot-distro login alpine --  bash <(curl -s https://raw.githubusercontent.com/rustinmyeye/ErgoNodeAndroid/master/alpine-ergo-node.sh)
-clear
-proot-distro login alpine 
-
-
+echo "
+apk add bash curl
+sh <(curl -s https://raw.githubusercontent.com/rustinmyeye/ErgoNodeAndroid/master/alpine-ergo-node.sh)
+" >> .alpinelinux_container/root/.profile
+./run-alpine.sh
