14578eb57758ff2b1dbead2e02a22adbbf3aacb9
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2025-03-05 09:46:20 -0500

    Create alpine-ergo-node.sh

---
+++ alpine-ergo-node.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#download things
+echo "Downloading stuff... please wait :)"
+#apk --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/ add android-tools --quiet
+apk add openjdk11 python3 wget tmux curl --quiet
+
+#create startup script
+cd ..
+cd etc
+cd profile.d 
+#curl https://raw.githubusercontent.com/rustinmyeye/ErgoNodeAndroid/master/ergo-startup.sh >> ergo.sh
+curl https://raw.githubusercontent.com/rustinmyeye/ErgoNodeAndroid/master/alpine-node-install.sh >> ergo.sh
+chmod +x ergo.sh
+clear
+./ergo.sh
