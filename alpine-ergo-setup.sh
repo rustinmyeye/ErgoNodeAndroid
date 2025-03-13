2e4b6f645391f0bdc4272d8e682482f8280e0956
rustinmyeye <118857376+rustinmyeye@users.noreply.github.com>
2025-03-13 08:50:51 -0400

    Update alpine-ergo-setup.sh

--- alpine-ergo-setup.sh
+++ alpine-ergo-setup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-curl -s https://raw.githubusercontent.com/rustinmyeye/ErgoNodeAndroid/refs/heads/master/init-alpine.sh >> init-alpine.sh
+
+if [ -f "./run-alpine.sh" ]; then
+    ./run-alpine.sh
+    exit
+fi
+
+curl -s https://raw.githubusercontent.com/rustinmyeye/ErgoNodeAndroid/refs/heads/master/init-alpine.sh -o init-alpine.sh
 chmod +x init-alpine.sh
-curl -s https://raw.githubusercontent.com/rustinmyeye/ErgoNodeAndroid/refs/heads/master/run-alpine.sh >> run-alpine.sh
+curl -s https://raw.githubusercontent.com/rustinmyeye/ErgoNodeAndroid/refs/heads/master/run-alpine.sh -o run-alpine.sh
 chmod +x run-alpine.sh
 ./init-alpine.sh
 clear
 echo "
 apk add bash curl
-sh <(curl -s https://raw.githubusercontent.com/rustinmyeye/ErgoNodeAndroid/master/alpine-ergo-node.sh)
+sh <(curl -s https://raw.githubusercontent.com/rustinmyeye/ErgoNodeAndroid/master/nipopow-ergo-node.sh)
 " >> .alpinelinux_container/root/.profile
 ./run-alpine.sh
